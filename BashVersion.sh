705ce32ea22d9316e3751c9d512bc3e756e4a6d6
xcutionr45 <jrs.daily@gmail.com>
2017-07-25 18:01:49 -0700

    Update and rename Bash Version.sh to BashVersion.sh

--- BashVersion.sh
+++ BashVersion.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-
-
 #echo "<result>`x='() { \:;}; echo VULNERABLE' sh -c :`</result>"
 
 echo "<result>`bash --version | cut -d " " -f4 | grep -v "Free"`</result>"
-
-
-#ea_display_name	Bash Version

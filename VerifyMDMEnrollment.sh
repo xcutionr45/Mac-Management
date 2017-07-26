dd533e25bb743ef981050475af7f51b039fc0513
xcutionr45 <jrs.daily@gmail.com>
2017-07-25 18:41:23 -0700

    Update and rename Verify MDM Enrollment.sh to VerifyMDMEnrollment.sh

--- VerifyMDMEnrollment.sh
+++ VerifyMDMEnrollment.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
-mdmEnrollmentProfileID="00000000-0000-0000-A000-4A414D460003"
+mdmEnrollmentProfileID="<YOUR MDM PROFILEID"
 enrolled=`/usr/bin/profiles -C | /usr/bin/grep "$mdmEnrollmentProfileID"`
 if [ "$enrolled" != "" ]; then
     echo "<result>Enrolled</result>"
 else
     echo "<result>Not Enrolled</result>"
 fi
-
-
-#ea_display_name	Verify MDM Enrollment

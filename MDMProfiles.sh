8e8e380b08b0f3d3122416f288e4bf74cfb35ba6
xcutionr45 <jrs.daily@gmail.com>
2017-07-25 18:18:44 -0700

    Update and rename MDM Profiles.sh to MDMProfiles.sh

--- MDMProfiles.sh
+++ MDMProfiles.sh
@@ -2,8 +2,3 @@
 
 PROFILES_SYSTEM=$(profiles -P | sed \$d | awk '{print $NF}' | tr ' ' '\n' | uniq -u | sort)
 echo "<result>$(echo $PROFILES_SYSTEM | tr ' ' '\n')</result>"
-
-exit 0
-
-
-#ea_display_name	MDM Profiles

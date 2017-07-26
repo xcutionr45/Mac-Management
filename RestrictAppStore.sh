b8b28abecc356138c73541a2a2199c73e1b3a7d2
xcutionr45 <jrs.daily@gmail.com>
2017-07-25 17:57:00 -0700

    Create RestrictAppStore.sh

---
+++ RestrictAppStore.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#SystemPref=`defaults read /Library/Preferences/com.apple.appstore restrict-store-softwareupdate-only`
+#ManagedPref=`defaults read /Library/Managed\ Preferences/com.apple.appstore restrict-store-softwareupdate-only`

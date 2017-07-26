17d3e53bfa9abf8403e43ef0ee5a47db2f64df75
xcutionr45 <jrs.daily@gmail.com>
2017-07-25 18:42:21 -0700

    Update VerifyMDMEnrollment.sh

--- VerifyMDMEnrollment.sh
+++ VerifyMDMEnrollment.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-mdmEnrollmentProfileID="<YOUR MDM PROFILEID"
+mdmEnrollmentProfileID="<YOUR MDM PROFILEID>"
 enrolled=`/usr/bin/profiles -C | /usr/bin/grep "$mdmEnrollmentProfileID"`
 if [ "$enrolled" != "" ]; then
     echo "<result>Enrolled</result>"
